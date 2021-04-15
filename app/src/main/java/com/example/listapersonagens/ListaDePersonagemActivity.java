4fa0f72448e78928b8d80b9e5f6d1d3224b05e58
BigRodri <rodrigonnes@icloud.com>
2021-04-15 18:28:27 -0300

    atualização

---
+++ app/src/main/java/com/example/listapersonagens/ListaDePersonagemActivity.java
@@ -0,0 +1,38 @@
+package com.example.listapersonagens;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaDePersonagemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_personagem);
+
+        List<String> personagem = new ArrayList<>(Arrays.asList("Neo", "Leon", "Chun-Li", "Donatello"));
+
+        ListView ListaDePersonagens = findViewById(R.id.Activity_main_lista_personagem);
+        ListaDePersonagens.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, personagem));
+
+
+        /*TextView primeiroPersonagem = findViewById(R.id.textView);
+        primeiroPersonagem.setText(personagem.get(0));
+
+        TextView segundoPersonagem = findViewById(R.id.textView2);
+        segundoPersonagem.setText(personagem.get(1));
+
+        TextView terceiroPersonagem = findViewById(R.id.textView3);
+        terceiroPersonagem.setText(personagem.get(2));*/
+
+
+
+    }
+}
